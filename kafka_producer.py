800db34d59708afe2302a3061a7104a24f5491a7
PeroxideParadox <118070626+PeroxideParadox@users.noreply.github.com>
2024-05-03 12:16:35 +0530

    Graphical Based Visualization

--- kafka_producer.py
+++ kafka_producer.py
@@ -10,7 +10,7 @@ topic_name = 'test1'
 fake = Faker()
 
 while True:
-    # Generating real time Random Data
+    
     co2_data = {
         'source': random.choice(['Factory', 'Vehicle', 'Power Plant', 'Agriculture', 'Construction', 'Shipping', 'Deforestation']),
         'co2_level': random.randint(300, 1000),  # 300 and 1000 ppm
@@ -18,11 +18,10 @@ while True:
         'timestamp': time.strftime('%Y-%m-%d %H:%M:%S')
     }
     
-    #converting the dictionary into JSON format 
-    json_co2_data = json.dumps(co2_data)
-
-    producer.send(topic_name, value=json_co2_data)
-    print(f"Produced: {json_co2_data}")
+    csv_like_data = f"{co2_data['source']},{co2_data['co2_level']},{co2_data['location']},{co2_data['timestamp']}"
+    
+    producer.send(topic_name, value=csv_like_data)
+    print(f"Produced: {csv_like_data}")
     time.sleep(3)
 
 producer.close()
