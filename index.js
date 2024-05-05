12121d01006f969b516223a0e61629efa6959c5c
PeroxideParadox <118070626+PeroxideParadox@users.noreply.github.com>
2024-05-05 14:31:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const csv = require('csv-parser');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const app = express();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/data', (req, res) => {
-    const data = [];
-    fs.createReadStream('kafka_data.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-            data.push(row);
-        })
-        .on('end', () => {
-            res.json(data);
-        })
-        .on('error', (err) => {
-            console.error('Error reading CSV file:', err);
-            res.status(500).json({ error: 'Internal Server Error' });
-        });
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
